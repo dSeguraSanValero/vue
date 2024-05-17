44bd8b6063e7db55091b93e8628bc536e3a74f2f
dSeguraSanValero <a27238@svalero.com>
2024-05-17 18:59:59 +0200

    Otro commit

--- src/views/PrivateView.vue
+++ src/views/PrivateView.vue
@@ -1,5 +1,19 @@
 <template>
-  <p v-if="userData">¡Bienvenido {{ userData.name }}!</p>
+    <p v-if="userData">¡Bienvenido {{ userData.name }}!</p>
+
+    <v-btn variant="outlined" @click="verReservas">Ver Reservas</v-btn>
+
+    <v-row>
+        <v-col cols="12" md="6" v-for="booking in bookings" :key="booking.bookingId">
+            <v-card>
+            <v-card-text>
+                <p>Origen: {{ booking.origin }}</p>
+                <p>Destino: {{ booking.destination }}</p>
+            </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+
 </template>
 
 <script lang="ts">
@@ -11,11 +25,37 @@
         name: 'ZonaPrivadaUser',
         setup() {
             const userStore = useUserStore();
+            const bookings = ref([] as any[]);
             const router = useRouter();
 
             return {
-                userData: userStore.userData
+                userData: userStore.userData,
+                router,
+                origin: "",
+                destination: "",
+                bookings
             };
+        },
+
+        methods: {
+            verReservas() {
+                const userStore = useUserStore();
+                const userId = userStore.userData?.Id;
+
+                fetch(`http://localhost:7238/Treatment?patientId=${userId}`)
+                .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+                })
+                .then(data => {
+                this.bookings = data;
+                })
+                .catch(error => {
+                console.error('Hubo un problema con la operación de fetch:', error);
+                });
+            }
         }
     });
 </script>
